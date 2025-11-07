e0997b107d4c3429c70cad05a2afed902148ff06
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 18:58:12 +0700

    membuat navhost ketika memulai akan dimulai dari welcomescreen

--- app/src/main/java/com/example/questnavigasitugas_067/navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/navigasi.kt
@@ -1,8 +1,11 @@
 package com.example.questnavigasitugas_067
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -16,5 +19,12 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Welcome.name,
+            modifier = Modifier.padding(isiRuang)
+        ){
 
+        }
 }
