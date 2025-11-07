2f5f1ca89d46ebb913ba96cb1bbe783b8dca5d9d
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:30:06 +0700

    membuat scaffold tampil data dan diberi warna teal_700

--- app/src/main/java/com/example/questnavigasitugas_067/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/view/TampilData.kt
@@ -1,6 +1,12 @@
 package com.example.questnavigasitugas_067.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.questnavigasitugas_067.R
 
@@ -14,5 +20,21 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Laki - Laki"),
         Pair(stringResource(id = R.string.alamat), "Jauh"),
         Pair(stringResource(id = R.string.status_perkawinan), "Lajang")
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_700)
+                )
+            )
+        }
     )
 }
