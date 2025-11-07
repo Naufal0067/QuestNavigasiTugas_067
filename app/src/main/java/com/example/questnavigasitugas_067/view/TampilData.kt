fb18355ad5235b8ff64cd6710047f6c1145696bb
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:28:00 +0700

    membuat val items untuk list nama lengkap, jenis kelamin, alamat, status perkawinan

--- app/src/main/java/com/example/questnavigasitugas_067/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/view/TampilData.kt
@@ -1,9 +1,18 @@
 package com.example.questnavigasitugas_067.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasitugas_067.R
 
 @Composable
 fun TampilData(
     onHomeClick: () -> Unit,
     onFormClick: () -> Unit
-){}
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Naufal"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Laki - Laki"),
+        Pair(stringResource(id = R.string.alamat), "Jauh"),
+        Pair(stringResource(id = R.string.status_perkawinan), "Lajang")
+    )
+}
