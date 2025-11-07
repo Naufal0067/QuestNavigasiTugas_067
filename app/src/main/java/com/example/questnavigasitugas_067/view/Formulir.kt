de2b59db40af0573d3a7add6fbf2f0723d552187
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:38:17 +0700

    membuat var text nama, alamt, JK, status kawin, dan val untuk gender dan status kawin

--- app/src/main/java/com/example/questnavigasitugas_067/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/view/Formulir.kt
@@ -2,12 +2,32 @@ package com.example.questnavigasitugas_067.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasitugas_067.R
 
 @Composable
 fun FormDataDiri(
     modifier: Modifier = Modifier.fillMaxSize(),
     onSubmit: () -> Unit
 ) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatusKawin by remember { mutableStateOf("") }
 
+    val genderOptions = listOf(
+        stringResource(R.string.jk_laki_laki),
+        stringResource(R.string.jk_perempuan)
+    )
+
+    val statusKawinOptions = listOf(
+        stringResource(R.string.sk_janda),
+        stringResource(R.string.sk_lajang),
+        stringResource(R.string.sk_duda)
+    )
 }
