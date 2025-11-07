b20d235467b60a8494d54bdebe553f09a9b552bf
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:23:57 +0700

    membuat scaffold untuk welcome

--- app/src/main/java/com/example/questnavigasitugas_067/view/welcomescreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/view/welcomescreen.kt
@@ -1,10 +1,27 @@
 package com.example.questnavigasitugas_067.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WelcomeScreen(
     onSubmitClick: () -> Unit
 ) {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Welcome") },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFF008577),
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    )
 }
