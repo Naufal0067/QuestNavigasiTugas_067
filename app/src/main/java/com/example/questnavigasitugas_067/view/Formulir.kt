c4a32b07bfc33363cb173d3337136e2733570105
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:36:34 +0700

    membuat file Formulir.kt dan function untuk form data diri/pendaftaran

---
+++ app/src/main/java/com/example/questnavigasitugas_067/view/Formulir.kt
@@ -0,0 +1,13 @@
+package com.example.questnavigasitugas_067.view
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(
+    modifier: Modifier = Modifier.fillMaxSize(),
+    onSubmit: () -> Unit
+) {
+
+}
