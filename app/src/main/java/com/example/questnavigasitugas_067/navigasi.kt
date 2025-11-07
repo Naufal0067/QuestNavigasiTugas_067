9f601ad57d2d4a1ecfeb124cd80c1ec3215bd965
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 18:55:52 +0700

    membuat function dataapp

--- app/src/main/java/com/example/questnavigasitugas_067/navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_067/navigasi.kt
@@ -1,7 +1,20 @@
 package com.example.questnavigasitugas_067
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Welcome,
     Formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+
 }
