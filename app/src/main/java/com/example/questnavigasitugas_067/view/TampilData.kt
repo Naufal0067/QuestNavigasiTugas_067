f92bd7f18ea50c88a7f2db9f12ffbf442852962e
Naufal0067 <naufalopang1928@gmail.com>
2025-11-07 19:26:57 +0700

    membuat file TampilData.kt dan membuat function untuk tampil data

---
+++ app/src/main/java/com/example/questnavigasitugas_067/view/TampilData.kt
@@ -0,0 +1,9 @@
+package com.example.questnavigasitugas_067.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+    onHomeClick: () -> Unit,
+    onFormClick: () -> Unit
+){}
